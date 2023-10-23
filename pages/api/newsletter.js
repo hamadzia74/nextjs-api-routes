872ec5483d2d4be9b2f37da8970b45ddbe0ca953
HAMAD ZIA <hamadzia74@gmail.com>
2023-10-23 22:54:18 +0500

    Worked to run the MongoDB Queries from Inside API

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,11 +1,18 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const userEmail = req.body;
         if (!userEmail || !userEmail.includes('@')) {
             res.status(422).json({ message: 'Invalid email address.' });
             return;
         }
-        console.log(userEmail);
+        const client = await MongoClient.connect('mongodb+srv://hamadzia74:WuQxY35zxbCL4bie@meetupapi.h7ydopx.mongodb.net/?retryWrites=true&w=majority')
+
+        const db = client.db();
+        await db.collection('emails').insertOne({ email: userEmail });
+        client.close();
+        // console.log(userEmail);
         res.status(201).json({ message: 'Signed up!' })
     }
 }
