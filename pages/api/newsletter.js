1716986fd2b7f769b0bb3f96c66cb868be43390a
Hamad Zia <77169231+hamadzia74@users.noreply.github.com>
2023-10-23 23:05:26 +0500

    Update newsletter.js

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -7,7 +7,7 @@ async function handler(req, res) {
             res.status(422).json({ message: 'Invalid email address.' });
             return;
         }
-        const client = await MongoClient.connect('mongodb+srv://hamadzia74:WuQxY35zxbCL4bie@meetupapi.h7ydopx.mongodb.net/?retryWrites=true&w=majority')
+        const client = await MongoClient.connect('mongodb+srv://<username>:<password>@meetupapi.h7ydopx.mongodb.net/?retryWrites=true&w=majority')
 
         const db = client.db();
         await db.collection('emails').insertOne({ email: userEmail });
