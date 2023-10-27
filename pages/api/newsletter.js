876638e9a0e5efbe234e3f8758bf1993ca239cc7
HAMAD ZIA <hamadzia74@gmail.com>
2023-10-27 23:01:19 +0500

    Worked on a new Create Context

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,7 +1,7 @@
 import {connectDatabase, inserDocument} from '../../helpers/db-utils';
 
 // async function connectDatabase() {
-//     const client = await MongoClient.connect('mongodb+srv://hamadzia74:WuQxY35zxbCL4bie@meetupapi.h7ydopx.mongodb.net/?retryWrites=true&w=majority')
+//     const client = await MongoClient.connect('mongodb+srv://<username>:<password>@meetupapi.h7ydopx.mongodb.net/?retryWrites=true&w=majority')
 //     return client;
 // }
 
@@ -18,7 +18,7 @@ async function handler(req, res) {
             res.status(422).json({ message: 'Invalid email address.' });
             return;
         }
-        // const client = await MongoClient.connect('mongodb+srv://hamadzia74:WuQxY35zxbCL4bie@meetupapi.h7ydopx.mongodb.net/events?retryWrites=true&w=majority')
+        // const client = await MongoClient.connect('mongodb+srv://<username>:<password>@meetupapi.h7ydopx.mongodb.net/events?retryWrites=true&w=majority')
 
         let client;
         try {
---
+++ store/notification-context.js
@@ -0,0 +1,16 @@
+import { createContext } from 'react';
+
+const NotificationContext = createContext({
+    notification: null,
+    showNotification: function () { },
+    hideNotification: function () { }
+});
+
+export function NotificationContextProvider(props) {
+    return (
+        <NotificationContext.Provider>
+            {props.children}
+        </NotificationContext.Provider>
+    )
+}
+export default NotificationContext;
